9ebfda9c9e5b24caec00015d876b106dedc5da43
yuhangc <cccumg@gmail.com>
2018-02-27 20:24:05 -0800

    added robustness test

--- scripts/IRL/irl_prediction_th.py
+++ scripts/IRL/irl_prediction_th.py
@@ -223,6 +223,30 @@ def predict_single_trajectory(predictor, path, id):
     plt.show()
 
 
+def plot_prediction(xh, xr, x_opt, x_goal, obs_data, ax, flag_legend=True, flag_marker=True):
+    if flag_marker:
+        ax.plot(xh[:, 0], xh[:, 1], '--ok', lw=1, fillstyle="none", label="measured")
+        ax.plot(x_opt[:, 0], x_opt[:, 1], '-ok', lw=2, fillstyle="none", label="predicted")
+        ax.plot(xr[:, 0], xr[:, 1], '-or', lw=1, fillstyle="none", label="robot")
+
+        ax.plot(x_goal[0], x_goal[1], 'ok', markersize=10)
+        ax.plot(xr[-1, 0], xr[-1, 1], 'or', markersize=10)
+        ax.plot(obs_data[0], obs_data[1], 'ob', markersize=10)
+
+        thalf = len(x_opt) / 2
+        ax.plot(x_opt[thalf, 0], x_opt[thalf, 1], 'ok')
+        ax.plot(xr[thalf, 0], xr[thalf, 1], 'or')
+    else:
+        ax.plot(xh[:, 0], xh[:, 1], '-', color="grey", lw=1, label="measured")
+        ax.plot(x_opt[:, 0], x_opt[:, 1], '-k', lw=1, label="predicted")
+        ax.plot(xr[:, 0], xr[:, 1], '-r', lw=1, label="robot")
+
+    ax.axis("equal")
+
+    if flag_legend:
+        ax.legend()
+
+
 def predict_and_save_all(predictor, path, save_path, user_list, cond, n_trial):
     for usr in user_list:
         path_full = path + "/user" + str(usr) + "/processed/" + cond
@@ -252,23 +276,74 @@ def predict_and_save_all(predictor, path, save_path, user_list, cond, n_trial):
 
             # plot and save the trajectory
             fig, axes = plt.subplots()
-            axes.plot(xh[:, 0], xh[:, 1], '--ok', lw=1, fillstyle="none", label="measured")
-            axes.plot(x_opt[:, 0], x_opt[:, 1], '-ok', lw=2, fillstyle="none", label="predicted")
-            axes.plot(xr[:, 0], xr[:, 1], '-or', lw=1, fillstyle="none", label="robot")
-
-            axes.plot(x_goal[0], x_goal[1], 'ok', markersize=10)
-            axes.plot(xr[-1, 0], xr[-1, 1], 'or', markersize=10)
-            axes.plot(obs_data[0], obs_data[1], 'ob', markersize=10)
-
-            thalf = len(x_opt) / 2
-            axes.plot(x_opt[thalf, 0], x_opt[thalf, 1], 'ok')
-            axes.plot(xr[thalf, 0], xr[thalf, 1], 'or')
-
-            axes.axis("equal")
-            axes.legend()
+            plot_prediction(xh, xr, x_opt, x_goal, obs_data, axes)
 
             # plt.show()
             fig.savefig(save_path + "/" + cond + "/user" + str(usr) + "_demo" + str(id) + ".png")
+
+
+def test_param_robustness(weights, predictor, path, trial):
+    # load the data
+    init_data = np.loadtxt(path + "/init.txt", delimiter=",")
+    goal_data = np.loadtxt(path + "/goal.txt", delimiter=",")
+    obs_data = np.loadtxt(path + "/obs.txt", delimiter=',')
+
+    traj = np.loadtxt(path + "/block" + str(trial) + ".txt", delimiter=",")
+
+    xh = traj[:, 0:4]
+    uh = traj[:, 4:6]
+    xr = traj[:, 6:9]
+    ur = traj[:, 9:11]
+
+    # extract common info
+    x0 = init_data[trial, 0:4]
+    x_goal = np.zeros_like(x0)
+    x_goal[0:2] = goal_data[trial]
+
+    u0 = uh + 0.1 * np.random.randn(uh.shape[0], uh.shape[1])
+
+    # do a nominal prediction first
+    n_cumu = 5
+    n_term = 1
+    predictor.set_param(weights[0:n_cumu], weights[n_cumu:(n_cumu+n_term)])
+
+    x_opt, u_opt = predictor.predict_full(u0, x0, x_goal, xr, ur)
+
+    fig, axes = plt.subplots()
+    plot_prediction(xh, xr, x_opt, x_goal, obs_data, axes)
+    plt.show()
+
+    # loop through the parameters
+    feature_name = ["vel", "acc", "collisionHR1", "collisionHR2", "collisionObs", "goal"]
+    n_sample = 7
+
+    lb = [-1, -1, -1, -1, -1, -1]
+    ub = [1, 1, 1, 1, 1, 1]
+
+    fig, axes = plt.subplots(len(weights), n_sample)
+    for i in range(len(weights)):
+        print "testing paremeter: " + feature_name[i]
+        for k, wi in enumerate(np.logspace(lb[i], ub[i], n_sample)*weights[i]):
+            w_test = weights
+            w_test[i] = wi
+
+            print w_test
+            predictor.set_param(w_test[0:n_cumu], w_test[n_cumu:(n_cumu+n_term)])
+
+            u0 = uh + 0.1 * np.random.randn(uh.shape[0], uh.shape[1])
+            x_opt, u_opt = predictor.predict_full(u0, x0, x_goal, xr, ur)
+
+            plot_prediction(xh, xr, x_opt, x_goal, obs_data, axes[i, k],
+                            flag_legend=False, flag_marker=False)
+            axes[i, k].set_title("w_" + feature_name[i] + " = " + "{:.3f}".format(wi))
+
+    plt.tick_params(
+        axis='x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='off') # labels along the bottom edge are off
+    plt.show()
 
 
 def traj_dist_avg(x1, x2):
@@ -287,14 +362,12 @@ if __name__ == "__main__":
     # predictor = IRLPredictor(dyn, [0.5, 10])
     predictor = IterativePredictor(dyn, [0.5, 10])
 
-    predict_and_save_all(predictor,
-                         "/home/yuhang/Documents/irl_data/winter18",
-                         "/home/yuhang/Documents/irl_data/winter18/figures",
-                         [0, 1, 2, 3], "rp", 20)
+    # predict_and_save_all(predictor,
+    #                      "/home/yuhang/Documents/irl_data/winter18",
+    #                      "/home/yuhang/Documents/irl_data/winter18/figures",
+    #                      [0, 1, 2, 3], "rp", 20)
 
-    # print "predicting user 0"
-    # predict_single_trajectory(predictor, "/home/yuhang/Documents/irl_data/winter18/user0/processed/rp", 0)
-    # predict_single_trajectory(predictor, "/home/yuhang/Documents/irl_data/winter18/user0/processed/rp", 1)
-    # predict_single_trajectory(predictor, "/home/yuhang/Documents/irl_data/winter18/user0/processed/rp", 2)
-    # predict_single_trajectory(predictor, "/home/yuhang/Documents/irl_data/winter18/user0/processed/rp", 3)
-    # predict_single_trajectory(predictor, "/home/yuhang/Documents/irl_data/winter18/user0/processed/rp", 4)
+    test_param_robustness(np.array([7.0, 20.0, 1.5, 0.0, 1.2, 45.0]), predictor,
+                          "/home/yuhang/Documents/irl_data/winter18/user0/processed/hp", 0)
+    test_param_robustness(np.array([7.0, 20.0, 1.5, 1.0, 1.2, 45.0]), predictor,
+                          "/home/yuhang/Documents/irl_data/winter18/user0/processed/rp", 0)
